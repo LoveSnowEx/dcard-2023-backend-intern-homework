40a85fde009f238b2b9f34e2f7a9d94aa30dd6bd
LoveSnowEx <lsktw22@gmail.com>
2023-03-16 14:14:21 +0800

    Listen grpc and wait signal

--- main.go
+++ main.go
@@ -1,12 +1,41 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/LoveSnowEx/dcard-2023-backend-intern-homework/db"
 	"github.com/LoveSnowEx/dcard-2023-backend-intern-homework/internal/router"
+	"github.com/LoveSnowEx/dcard-2023-backend-intern-homework/proto/grpc/pagelistServer"
+	"github.com/LoveSnowEx/dcard-2023-backend-intern-homework/proto/pb"
 	"github.com/gofiber/fiber/v2"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
+
+func runFiber(addr string) error {
+	app := fiber.New()
+	router.Setup(app)
+
+	return app.Listen(addr)
+}
+
+func runGprc(addr string) error {
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	srv := grpc.NewServer()
+	reflection.Register(srv)
+	pb.RegisterPageListServiceServer(srv, &pagelistServer.Server{})
+
+	return srv.Serve(lis)
+}
 
 func main() {
 	_, err := db.Connect()
@@ -21,13 +50,28 @@ func main() {
 		}
 	}()
 
-	app := fiber.New()
+	c := make(chan os.Signal, 1)
+	done := make(chan struct{}, 1)
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World 👋!")
-	})
+	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
 
-	router.Setup(app)
+	go func() {
+		sig := <-c
+		fmt.Println()
+		fmt.Println(sig)
+		done <- struct{}{}
+	}()
+	go func() {
+		log.Fatalln(runFiber(":3000"))
+		done <- struct{}{}
+	}()
+	go func() {
+		log.Fatalln(runGprc(":50051"))
+		done <- struct{}{}
+	}()
 
-	app.Listen(":3000")
+	fmt.Println("waiting for Ctrl+C signal")
+
+	<-done
+	log.Println("Closing ")
 }
