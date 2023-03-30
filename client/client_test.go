a6482342d60e9b067392b2911fa31319b50170e9
LoveSnowEx <lsktw22@gmail.com>
2023-03-30 07:24:18 +0000

    Add test for grpc api test New, Delete, Begin, End, PushBack, PushFront

---
+++ client/client_test.go
@@ -0,0 +1,213 @@
+package client
+
+import (
+	"container/list"
+	"context"
+	"log"
+	"math/rand"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/LoveSnowEx/dcard-2023-backend-intern-homework/proto/pb"
+	"google.golang.org/grpc"
+)
+
+var (
+	c pb.PageListServiceClient
+)
+
+func TestMain(m *testing.M) {
+	conn, err := grpc.Dial(":50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("failed to dial: %v", err)
+	}
+	defer conn.Close()
+	c = pb.NewPageListServiceClient(conn)
+
+	m.Run()
+}
+
+func TestNewList(t *testing.T) {
+	res, err := c.New(context.Background(), &pb.Empty{})
+	require.NoError(t, err)
+	require.NotNil(t, res)
+}
+
+func TestListEnd(t *testing.T) {
+	res, err := c.New(context.Background(), &pb.Empty{})
+	require.NoError(t, err)
+
+	it, err := c.End(context.Background(), &pb.EndRequest{ListKey: res.Key})
+	require.NoError(t, err)
+	require.NotNil(t, it)
+}
+
+func TestListBegin(t *testing.T) {
+	res, err := c.New(context.Background(), &pb.Empty{})
+	require.NoError(t, err)
+
+	itBegin, err := c.Begin(context.Background(), &pb.BeginRequest{ListKey: res.Key})
+	require.NoError(t, err)
+	require.NotNil(t, itBegin)
+
+	itEnd, err := c.End(context.Background(), &pb.EndRequest{ListKey: res.Key})
+	require.NoError(t, err)
+	require.NotNil(t, itEnd)
+
+	require.Equal(t, itBegin.Key, itEnd.Key)
+}
+
+func CompareList(t *testing.T, l *list.List, listKey string) {
+	it, err := c.Begin(context.Background(), &pb.BeginRequest{ListKey: listKey})
+	require.NoError(t, err)
+	require.NotNil(t, it)
+
+	itEnd, err := c.End(context.Background(), &pb.EndRequest{ListKey: listKey})
+	require.NoError(t, err)
+	require.NotNil(t, itEnd)
+
+	for i, e := 0, l.Front(); e != nil; i, e = i+1, e.Next() {
+		require.Equal(t, e.Value, it.PageId)
+		it, err = c.Next(context.Background(), &pb.NextRequest{IterKey: it.Key})
+		require.NoError(t, err)
+		require.NotNil(t, it)
+	}
+
+	require.Equal(t, itEnd.Key, it.Key)
+}
+
+func TestListPushBack0(t *testing.T) {
+	res, err := c.New(context.Background(), &pb.Empty{})
+	require.NoError(t, err)
+
+	l := list.New()
+
+	CompareList(t, l, res.Key)
+}
+
+func TestListPushBack1(t *testing.T) {
+	res, err := c.New(context.Background(), &pb.Empty{})
+	require.NoError(t, err)
+
+	l := list.New()
+
+	val := uint32(1)
+
+	l.PushBack(val)
+
+	it, err := c.PushBack(context.Background(), &pb.PushRequest{ListKey: res.Key, PageId: val})
+	require.NoError(t, err)
+	require.NotNil(t, it)
+
+	CompareList(t, l, res.Key)
+}
+
+func TestListPushBack100(t *testing.T) {
+	res, err := c.New(context.Background(), &pb.Empty{})
+	require.NoError(t, err)
+
+	l := list.New()
+
+	for i := 0; i < 100; i++ {
+		val := uint32(i + 1)
+
+		l.PushBack(val)
+
+		it, err := c.PushBack(context.Background(), &pb.PushRequest{ListKey: res.Key, PageId: val})
+		require.NoError(t, err)
+		require.NotNil(t, it)
+	}
+
+	CompareList(t, l, res.Key)
+}
+
+func TestListPushBackRandom100(t *testing.T) {
+	res, err := c.New(context.Background(), &pb.Empty{})
+	require.NoError(t, err)
+
+	l := list.New()
+
+	for i := 0; i < 100; i++ {
+		val := rand.Uint32()/1000 + 1
+
+		l.PushBack(val)
+
+		it, err := c.PushBack(context.Background(), &pb.PushRequest{ListKey: res.Key, PageId: val})
+		require.NoError(t, err)
+		require.NotNil(t, it)
+	}
+
+	CompareList(t, l, res.Key)
+}
+
+func TestListPushFront0(t *testing.T) {
+	res, err := c.New(context.Background(), &pb.Empty{})
+	require.NoError(t, err)
+
+	l := list.New()
+
+	CompareList(t, l, res.Key)
+}
+
+func TestListPushFront1(t *testing.T) {
+	res, err := c.New(context.Background(), &pb.Empty{})
+	require.NoError(t, err)
+
+	l := list.New()
+
+	val := uint32(1)
+
+	l.PushFront(val)
+
+	it, err := c.PushFront(context.Background(), &pb.PushRequest{ListKey: res.Key, PageId: val})
+	require.NoError(t, err)
+	require.NotNil(t, it)
+
+	CompareList(t, l, res.Key)
+}
+
+func TestListPushFront100(t *testing.T) {
+	res, err := c.New(context.Background(), &pb.Empty{})
+	require.NoError(t, err)
+
+	l := list.New()
+
+	for i := 0; i < 100; i++ {
+		val := uint32(i + 1)
+
+		l.PushFront(val)
+
+		it, err := c.PushFront(context.Background(), &pb.PushRequest{ListKey: res.Key, PageId: val})
+		require.NoError(t, err)
+		require.NotNil(t, it)
+	}
+
+	CompareList(t, l, res.Key)
+}
+
+func TestListPushFrontRandom100(t *testing.T) {
+	res, err := c.New(context.Background(), &pb.Empty{})
+	require.NoError(t, err)
+
+	l := list.New()
+
+	for i := 0; i < 100; i++ {
+		val := rand.Uint32()/1000 + 1
+
+		l.PushFront(val)
+
+		it, err := c.PushFront(context.Background(), &pb.PushRequest{ListKey: res.Key, PageId: val})
+		require.NoError(t, err)
+		require.NotNil(t, it)
+	}
+
+	CompareList(t, l, res.Key)
+}
+
+func TestDeleteList(t *testing.T) {
+	res, err := c.New(context.Background(), &pb.Empty{})
+
+	_, err = c.Delete(context.Background(), &pb.DeleteRequest{ListKey: res.Key})
+	require.NoError(t, err)
+}
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/gofiber/fiber/v2 v2.42.0
 	github.com/google/uuid v1.3.0
 	github.com/spf13/viper v1.15.0
+	github.com/stretchr/testify v1.8.1
 	google.golang.org/grpc v1.52.0
 	google.golang.org/protobuf v1.30.0
 	gorm.io/driver/postgres v1.4.8
@@ -15,6 +16,7 @@ require (
 
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/fullstorydev/grpcurl v1.8.6 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
@@ -33,6 +35,7 @@ require (
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
 	github.com/philhofer/fwd v1.1.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.4.4 // indirect
 	github.com/savsgio/dictpool v0.0.0-20221023140959-7bf2e61cea94 // indirect
 	github.com/savsgio/gotils v0.0.0-20230208104028-c358bd845dee // indirect
